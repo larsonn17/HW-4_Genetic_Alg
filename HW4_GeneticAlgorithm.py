ae2fb2e837473ff750fe592258c4e5838ad2d888
larsonn17 <larsonn17@up.edu>
2017-03-06 20:13:06 -0800

    base version working

--- HW4_GeneticAlgorithm.py
+++ HW4_GeneticAlgorithm.py
@@ -41,14 +41,12 @@ class AIPlayer(Player):
     def __init__(self, inputPlayerId):
         super(AIPlayer,self).__init__(inputPlayerId, "SPORE")
 
-        #list of lists to store our current population of genes
+        #list of genes to store our current population of genes
+        #each gene containts 11 pair of numbers that represent the layout coordinates
         self.curGenePop = []
 
         #list to store fitness of each gene in the population
         self.geneFitnessScores = []
-
-        #index to track which gene in population is next to be evaluated
-        self.currentGene = 0
 
         #records number of games agent has played
         self.gamesPlayed = 0
@@ -77,13 +75,15 @@ class AIPlayer(Player):
     #Return: The coordinates of where the construction is to be placed
     ##
     def getPlacement(self, currentState):
+        #create an initial set of random genes if we dont have any
         if not self.curGenePop:
             self.initGenePop()
+
         moves = []
         if currentState.phase == SETUP_PHASE_1:    #stuff on my side
-            print self.gamesPlayed
-            print self.curGenePop
-            print "new line"
+            #print self.gamesPlayed
+            #print self.curGenePop
+            #print "new line"
             for coords in self.curGenePop[self.gamesPlayed]:
                 moves.append(coords)
             return moves
@@ -118,27 +118,29 @@ class AIPlayer(Player):
     # Return: None
     # #
     def initGenePop(self):
-        numToPlace = 11
         index = 0 #counter for constructs and grass we will place
         count = 0 #counter for gene indexes of population list
+        tempList = [] #holds the coordinates in a list
 
-        for count in range(0, popSize):
-            if index < numToPlace:
+        while count < popSize:
+            if index < 11:
                 #Choose any x location
                 x = random.randint(0, 9)
                 #Choose any y location on your side of the board
                 y = random.randint(0, 3)
                 tup = (x, y)
-                print tup
-                self.curGenePop[count].append(tup)
-                for x,y in itertools.combinations(self.curGenePop[count], 2):
-                    if cmp(x,y) != 0: #check that x and y are not the same coords
-                        index += 1
-
-            if len(self.curGenePop[count]) > 11:
-                print "ERROR, initial list too big"
-
-            if index > 11:
+                tempList.append(tup)
+                badCoords = False
+                for x,y in itertools.combinations(tempList, 2):
+                    if cmp(x,y) == 0: #check that x and y are not the same coords
+                        badCoords = True
+                if badCoords:#remove the just added index as it was redundant
+                    tempList.pop()
+                else:
+                    index += 1
+            else:
+                self.curGenePop.append(tempList)
+                tempList = [] #empty the the tempList so it can be used again
                 count += 1
                 index = 0
 
@@ -157,60 +159,52 @@ class AIPlayer(Player):
         geneSplitPos = random.randint(0, len(parent1))
 
         #mate the two parents thru crossover
-        index = random.int(0,1)
-        if index == 0:
+        mateOrder = random.randint(0,1)
+        if mateOrder == 0:
             child = parent1[:geneSplitPos] + parent2[geneSplitPos:]
         else:
             child = parent2[:geneSplitPos] + parent1[geneSplitPos:]
 
+        #make sure there aren't any duplicates
+        #if there is a duplicate, generate a new random value for the 2nd occurance,
+        #this also serves as a form of random mutation
         for a, b in itertools.combinations(child, 2):
             if cmp(a,b) == 0:
                 print "matching coordinates"
+                print "A =" + str(a)
+                print "B =" + str(b)
+                #Choose any x location
+                x = random.randint(0, 9)
+                #Choose any y location on your side of the board
+                y = random.randint(0, 3)
+                b = (x, y)
+                print "New B =" + str(b)
+        #tempList.append(tup)
+        #badCoords = False
+        #for x,y in itertools.combinations(tempList, 2):
+        #    if cmp(x,y) == 0: #check that x and y are not the same coords
+        #        badCoords = True
+        #if badCoords:#remove the just added index as it was redundant
+        #    tempList.pop()
+        #else:
+        #    index += 1
+
 
         return child
 
-    ##
-    # generateNextGen
-    # Description: Produces next generation of genes from the current population
-    #
-    # Parameters:
-    #   population - the population of genes
-    def generateNextGen(self, population):
-        #Reset the child populations
-        childPop = []
-        #use the longest game as the best (100% mating chance)
-        localPerfect = self.mostTurnsTaken * 2
-        #reset most turns taken
-        self.mostTurnsTaken = 0
-        size = len(population)
-
-        while count < popSize:
-            parent1Index = random.randInt(0, popSize)
-            parent2Range = range(0,parent1Index) + range(parent1Index, popSize)
-            parent2Index = choice(parent2Range)
-            if(parent1Index == parent2Index):
-                print "Error, parents indexes are the same"
-                break
-            parent1 = population[parent1Index]
-            parent2 = population[parent2Index]
-
-            summedScores = self.geneFitnessScores[parent1Index] + self.geneFitnessScores[parent2Index]
-            passingScore = random(0, localPerfect)
-            if summedScores > passingScore:
-                count += 1
-                childPop.append(self.mateParents(parent1, parent2))
-
-        #return the new population
-        return childPop
 
     ##
     #evaluateFitness
     #
     #Description: Looks at the gene and determines its fitness score
     #
+    #Parameters:
+    #   turns - an integer value that indicats the number of turns taken
+    #           by our ai before its epic defeat at the hands of booger
+    #
     #Return:
     #       score: the evaluation of the gene
-    def evaluateFitness(self):
+    def evaluateFitness(self, turns):
         score = self.turnCounter
         if score > self.mostTurnsTaken:
             self.mostTurnsTaken = score
@@ -233,12 +227,13 @@ class AIPlayer(Player):
         moves = listAllLegalMoves(currentState)
         selectedMove = moves[random.randint(0,len(moves) - 1)];
 
+
         #don't do a build move if there are already 3+ ants
         numAnts = len(currentState.inventories[currentState.whoseTurn].ants)
         while (selectedMove.moveType == BUILD and numAnts >= 3):
             selectedMove = moves[random.randint(0,len(moves) - 1)];
 
-        if selectedMove == Move(End, None, None):
+        if selectedMove.moveType == END:
             self.turnCounter += 1
         return selectedMove
 
@@ -264,16 +259,60 @@ class AIPlayer(Player):
     #
     def registerWin(self, hasWon):
         #score the gene, add the score to the fitness list
-        self.geneFitnessScores[currentGene].append(self.evaluateFitness())
+        self.geneFitnessScores.append(self.evaluateFitness(self.turnCounter))
+        print "Fitness list " +str(self.geneFitnessScores)
         #increment number of finished games
         self.gamesPlayed += 1
+        print "Game Count " + str(self.gamesPlayed)
         #Reset the turn counter for the next game
+
+        print "Turns this Game " + str(self.turnCounter)
         self.turnCounter = 0
+
         if self.gamesPlayed == popSize:
             self.gamesPlayed = 0
-            #make new genes based on the better parents
-        else:
-            nextGene = self.curGenePop[self.gamesPlayed]
+            self.generateNextGen(self.curGenePop)
+
+    ##
+    # generateNextGen
+    # Description: Produces next generation of genes from the current population
+    #
+    # Parameters:
+    #   population - the population of genes
+    def generateNextGen(self, population):
+        #Reset the child populations
+        childPop = []
+        #use the longest game as the best score to compare the other parents against
+        localPerfect = self.mostTurnsTaken * 2
+        #reset most turns taken
+        self.mostTurnsTaken = 0
+
+        count = 0
+        while count < popSize:
+            parent1Index = random.choice(range(0, popSize))
+            parent2Index = random.choice(range(0, popSize))
+            while parent2Index == parent1Index:
+                parent2Range = random.choice(range(0, popSize))
+            #else:
+            #    parent2Range = range(0,parent1Index)
+            #parent2Index = random.choice(parent2Range)
+            if(parent1Index == parent2Index):
+                print "Error, parents indexes are the same for child gene #" + str(count)
+                print "parent indexs :" + str(parent1Index) + str(parent2Index)
+            parent1 = population[parent1Index]
+            parent2 = population[parent2Index]
+
+            summedScores = self.geneFitnessScores[parent1Index] + self.geneFitnessScores[parent2Index]
+            passingScore = random.choice(range(0, localPerfect))
+            print "passing score :" + str(passingScore)
+            print "summed score :" + str(summedScores)
+            if summedScores > passingScore:
+                count += 1
+                childPop.append(self.mateParents(parent1, parent2))
+        #reset the fitness scores
+        self.geneFitnessScores = []
+        #set the new child pop as the current gene population
+        self.curGenePop = childPop[:]
 
 ################################################################################
 ## UNIT TESTS
